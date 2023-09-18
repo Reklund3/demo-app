e7b9de2ee015d21825bada2c3c6572f1834e19e3
Robert Eklund <robert.eklund3@gmail.com>
2023-09-17 22:04:00 -0500

    WIP creating tauri command to fetch a post.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tonic_web::GrpcWebClientLayer;
+use hyper::Client;
+use hyper::client::HttpConnector;
+use tonic::body::BoxBody;
+use tonic_web::{GrpcWebCall, GrpcWebClientLayer, GrpcWebClientService};
+use crate::post_service::post_service_client::PostServiceClient;
 
 pub mod post_service {
     tonic::include_proto!("posts");
@@ -13,6 +17,27 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+struct GetPostError {
+    reason: String
+}
+
+// #[tauri::command]
+// // async fn get_post(id: &str, mut client: PostServiceClient<GrpcWebClientService<Client<HttpConnector, GrpcWebCall<BoxBody>>>>) -> Result<tonic::Response<post_service::Post>, tonic::Status> {
+// async fn get_post(id: &str, mut client: PostServiceClient<GrpcWebClientService<Client<HttpConnector, GrpcWebCall<BoxBody>>>>) -> Result <tonic::Response<post_service::Post>, GetPostError> {
+//
+// }
+
+// async fn get_post_call(id: &str, mut client: PostServiceClient<GrpcWebClientService<Client<HttpConnector, GrpcWebCall<BoxBody>>>>) -> Result <tonic::Response<post_service::Post>, GetPostError> {
+async fn get_post_call(id: &str, mut client: PostServiceClient<GrpcWebClientService<Client<HttpConnector, GrpcWebCall<BoxBody>>>>) {
+    let request = tonic::Request::new(post_service::GetPostRequest {
+        id: id.to_string(),
+    });
+
+    let response = client.get_post(request).await;
+
+    println!("RESPONSE={:?}", response.expect(""));
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = hyper::Client::builder().http2_only(true).build_http();
@@ -21,26 +46,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .layer(GrpcWebClientLayer::new())
         .service(client);
 
-    let mut client = post_service::post_service_client::PostServiceClient::with_origin(
+    let mut client: PostServiceClient<GrpcWebClientService<Client<HttpConnector, GrpcWebCall<BoxBody>>>> = PostServiceClient::with_origin(
         post_svc,
         "http://127.0.0.1:8080".try_into()?,
     );
 
-    let request = tonic::Request::new(post_service::GetPostRequest {
-        id: "00768482-f3ea-4a0e-b18d-d3008c838212".to_string(),
-    });
+    get_post_call("23c71bef-b92c-437c-a94e-599b03cf7501", client).await;
 
-    let response = client.get_post(request).await?;
-
-    println!("RESPONSE={:?}", response);
-
-    let request_two = tonic::Request::new(post_service::GetPostsRequest {
-        page_number: 0,
-        page_size: 25,
-    });
-    let response_two = client.get_posts(request_two).await?;
-
-    println!("RESPONSE={:?}", response_two);
+    // let request_two = tonic::Request::new(post_service::GetPostsRequest {
+    //     page_number: 0,
+    //     page_size: 25,
+    // });
+    // let response_two = client.get_posts(request_two).await?;
+    //
+    // println!("RESPONSE={:?}", response_two);
 
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])

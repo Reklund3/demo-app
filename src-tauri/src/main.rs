7feff32d2bb1f5731feb27372ce96ce92042bbbc
Robert Eklund <robert.eklund3@gmail.com>
2023-09-17 19:48:47 -0500

    baseline working grpc post client.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -13,15 +13,34 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let client = hyper::Client::builder().build_http();
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = hyper::Client::builder().http2_only(true).build_http();
 
     let post_svc = tower::ServiceBuilder::new()
         .layer(GrpcWebClientLayer::new())
         .service(client);
 
-    let mut client = post_service::post_service_client::PostServiceClient::with_origin(post_svc, "http://127.0.0.1:8080".try_into()?);
+    let mut client = post_service::post_service_client::PostServiceClient::with_origin(
+        post_svc,
+        "http://127.0.0.1:8080".try_into()?,
+    );
 
+    let request = tonic::Request::new(post_service::GetPostRequest {
+        id: "00768482-f3ea-4a0e-b18d-d3008c838212".to_string(),
+    });
+
+    let response = client.get_post(request).await?;
+
+    println!("RESPONSE={:?}", response);
+
+    let request_two = tonic::Request::new(post_service::GetPostsRequest {
+        page_number: 0,
+        page_size: 25,
+    });
+    let response_two = client.get_posts(request_two).await?;
+
+    println!("RESPONSE={:?}", response_two);
 
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
